52a9abc029b41417496bc17b9b2a6dc75f27263c
utsavdevsharma <utsav.sharma@joshworkz.com>
2013-05-25 17:28:24 +0530

    removing scripts no more required

--- js/readfile.js
+++
@@ -1,9 +0,0 @@
-console.error("@");
-var fs = require('fs');
-var lessCss = fs.readFile("../less/style.less", function read(err, data){
-    if(err) {
-        return console.error(err);
-    }
-    console.error(data+"#");
-});
-console.error(lessCss+"@");
